c64f76a5a22ad55143521e395c4237bd4bd354c8
Thangible <thangible@gmail.com>
2023-03-03 08:02:05 +0100

    implement augmentation, not tested)

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import albumentations as A
 NEWDIR = "../augmented_yolov5_dataset/"
 OLDDIR = "../yolov5_dataset"
 MASKDIR = '../yolov5_dataset/SegmentationObject'
+DATA_INFO = '../yolov5_dataset/manual_annotated_data.csv'
 
 Resize = A.Resize(height = 500, width = 500, interpolation=1, p=1.0)
 CenterCrop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0),Resize])
     
-create_augmented_images(augmentation = CenterCrop, old_dir = OLDDIR, new_dir = NEWDIR, mask_dir = MASKDIR)
+create_augmented_images(augmentation = CenterCrop, olddir = OLDDIR, newdir = NEWDIR, maskdir = MASKDIR, data_info_path = DATA_INFO)
 os.system('python3 train.py --data dataset.yaml --hyp custom_hyp.yaml --weights yolov5s.py --cache --epochs 300')
