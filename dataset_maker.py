25f772b6021f6dc7ca082ac6dca0815810effdf4
Thangible <thangible@gmail.com>
2023-03-03 08:17:10 +0100

    implement augmentation, not tested)

--- dataset_maker.py
+++ dataset_maker.py
@@ -7,11 +7,14 @@ import shutil
 import pandas as pd
 from sklearn import preprocessing
 import numpy as np
+import cv2
 
 def create_augmented_images(augmentation, olddir, newdir, maskdir, data_info_path):
     #create an empty director of new path with image/ train and image/test
-    newtrain = pathlib.Path(newdir,'images', 'train')
-    newtest = pathlib.Path(newdir, 'images', 'train')
+    newtrain_img = pathlib.Path(newdir,'images', 'train')
+    newtest_img = pathlib.Path(newdir, 'images', 'test')
+    newtrain_label = pathlib.Path(newdir, 'labels', 'train')
+    newtest_label = pathlib.Path(newdir, 'labels', 'test')
     odltrain = pathlib.Path(olddir,'images', 'train')
     oldtest = pathlib.Path(olddir, 'images', 'test')
     #read all images as files names into a varaible
@@ -27,28 +30,49 @@ def create_augmented_images(augmentation, olddir, newdir, maskdir, data_info_pat
     le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
     data_info['label'] = le.transform(label)
     #AUGMENT FOR TRAIN SET
-    for img_paths in [org_trainimages, org_testimages]:
-        for img_path in img_paths:
-            img = imread(img_path)        
-            #MASK
-            image_name = img_path.stem
-            mask_path = get_mask_path(image_name, maskdir)
-            mask = imread(mask_path)
-            #AUGMENTATION
-            augmented = augmentation(image = img, mask = mask)
-            augmented_mask = augmented['mask']
-            augmented_img = augmented['image']
-            #Process mask
-            coords  = get_bbox(augmented_mask)
-            cat_label = get_label(data_info, img_path.name)
-            label = le.transform([cat_label])[0]
-            bbox = [np.insert(coord,0, label) for coord in coords]
-            #NEW DIR
-            img_newpath = pathlib.Path(newdir, 'images', image_name + '.jpg')
-            mask_newpath = pathlib.Path(maskdir, 'labels', image_name + '.txt')
-            #save file
-            np.savetxt(mask_newpath, bbox, fmt='%d %.5f %.5f %.5f %.5f')
-            cv2.imwrite(img_newpath, augmented_img)
+    for img_path in org_trainimages:
+        img = imread(img_path)        
+        #MASK
+        image_name = img_path.stem
+        mask_path = get_mask_path(image_name, maskdir)
+        mask = imread(mask_path)
+        #AUGMENTATION
+        augmented = augmentation(image = img, mask = mask)
+        augmented_mask = augmented['mask']
+        augmented_img = augmented['image']
+        #Process mask
+        coords  = get_bbox(augmented_mask)
+        cat_label = get_label(data_info, img_path.name)
+        label = le.transform([cat_label])[0]
+        bbox = [np.insert(coord,0, label) for coord in coords]
+        #NEW DIR
+        img_newpath = pathlib.Path(newtrain_img, image_name + '.jpg')
+        label_newpath = pathlib.Path(newtrain_label, image_name + '.txt')
+        #save file
+        np.savetxt(label_newpath, bbox, fmt='%d %.5f %.5f %.5f %.5f')
+        cv2.imwrite(img_newpath, augmented_img)
+        
+    for img_path in org_testimages:
+        img = imread(img_path)        
+        #MASK
+        image_name = img_path.stem
+        mask_path = get_mask_path(image_name, maskdir)
+        mask = imread(mask_path)
+        #AUGMENTATION
+        augmented = augmentation(image = img, mask = mask)
+        augmented_mask = augmented['mask']
+        augmented_img = augmented['image']
+        #Process mask
+        coords  = get_bbox(augmented_mask)
+        cat_label = get_label(data_info, img_path.name)
+        label = le.transform([cat_label])[0]
+        bbox = [np.insert(coord,0, label) for coord in coords]
+        #NEW DIR
+        img_newpath = pathlib.Path(newtest_img, image_name + '.jpg')
+        label_newpath = pathlib.Path(newtest_label, image_name + '.txt')
+        #save file
+        np.savetxt(label_newpath, bbox, fmt='%d %.5f %.5f %.5f %.5f')
+        cv2.imwrite(img_newpath, augmented_img)
         
         
 def get_label(data_info, name):
