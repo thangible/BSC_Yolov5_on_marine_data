ad6a9a1d31f7a0b6c291b2cccc4bd137db45d278
Thangible <thangible@gmail.com>
2023-03-08 16:26:12 +0100

    update augmentation for yolov5

--- main.py
+++ main.py
@@ -10,21 +10,41 @@ DATA_INFO = '../yolov5_dataset/manual_annotated_data.csv'
 Resize = A.Resize(height = 500, width = 500, interpolation=1, p=1.0)
 CenterCrop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0),Resize])
 CenterCrop_2 = A.Compose([A.CenterCrop(height = 100, width = 100, p=1.0),Resize])
-GridDropout = A.GridDropout(ratio = 0.6, random_offset = True, p=1)
-CLAHE = A.CLAHE(clip_limit = 4, tile_grid_size=(4, 4), p=1.0)
+Rotation = A.Rotate(p=1.0)
+Flip = A.Flip(p=1.0)
+##MOTION BLUR
+#COLOR AUGMENTATION
+CLAHE = A.CLAHE(clip_limit = 16, tile_grid_size=(4, 4), p=1.0)
 Sharpen = A.Sharpen(alpha= (0.6,0.8), lightness = (0.6,1.0), p=1.0)
+ChannelShuffle = A.ChannelShuffle(p =1)
+ColorJitter =  A.ColorJitter(brightness=0.8, hue = 0.42, contrast = 0.54, saturation = 0.65, p=1.0)
 ToGray = A.ToGray(p=1)
 ToSepia = A.ToSepia(p=1)
 GaussNoise = A.GaussNoise(p =1)
-
-#BATCH2
-MotionBlur =  A.MotionBlur(blur_limit =31,  p=1.0)
 Normalize = A.Normalize(mean = (0.184, 1.289, 0.661), std = (0.708, 0.338, 0.177), p = 1.0)
+### SOLARIZE
+
+#CUTOUT
+GridDropout = A.GridDropout(ratio = 0.6, random_offset = True, p=1)
+Cutout = A.CoarseDropout(max_holes=1, p =1, max_height=50, max_width=50)
 
 
 aug_dict = {}
-aug_dict['MotionBlur'] = MotionBlur
+aug_dict['CenterCrop_2_of_5'] = CenterCrop
+aug_dict['CenterCrop_1_of_5'] = CenterCrop_2
+aug_dict['Rotation'] = Rotation
+aug_dict['Flip'] = Flip
+#COLOR
+aug_dict['CLAHE'] = CLAHE
+aug_dict['Sharpen'] = Sharpen
+aug_dict['ChannelShuffle'] = ChannelShuffle
+aug_dict['ToGray'] = ToGray
+aug_dict['ToSepia'] = ToSepia
+aug_dict['GaussNoise'] = GaussNoise
 aug_dict['Normalize'] = Normalize
+#Cutout
+aug_dict['GridDropout'] = GridDropout
+aug_dict['Cutout'] = Cutout
 
 
 # os.system('python3 train.py --data dataset_baseline.yaml --hyp custom_hyp.yaml --weights yolov5s.pt --cache --epochs 500 --run_name baseline')
