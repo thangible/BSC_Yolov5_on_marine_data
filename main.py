70c8d563c8cdf4f612f69093edbdbfbc8537b565
Thangible <thangible@gmail.com>
2023-03-10 00:04:40 +0100

    add more augmentation to main.py:

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dataset_maker import create_augmented_images
 import albumentations as A
+import time
 
 NEWDIR = "../augmented_yolov5_dataset/"
 OLDDIR = "../yolov5_dataset"
@@ -32,19 +33,19 @@ Cutout = A.CoarseDropout(max_holes=1, p =1, max_height=50, max_width=50)
 aug_dict = {}
 # aug_dict['CenterCrop_2_of_5'] = CenterCrop
 # aug_dict['CenterCrop_1_of_5'] = CenterCrop_2
-# aug_dict['Rotation'] = Rotation
-# aug_dict['Flip'] = Flip
-# #COLOR
-# aug_dict['CLAHE'] = CLAHE
-# aug_dict['Sharpen'] = Sharpen
-# aug_dict['ChannelShuffle'] = ChannelShuffle
-# aug_dict['ToGray'] = ToGray
-# aug_dict['ToSepia'] = ToSepia
-# aug_dict['GaussNoise'] = GaussNoise
-# aug_dict['Normalize'] = Normalize
-# #Cutout
-# aug_dict['GridDropout'] = GridDropout
-# aug_dict['Cutout'] = Cutout
+aug_dict['Rotation'] = Rotation
+aug_dict['Flip'] = Flip
+#COLOR
+aug_dict['CLAHE'] = CLAHE
+aug_dict['Sharpen'] = Sharpen
+aug_dict['ChannelShuffle'] = ChannelShuffle
+aug_dict['ToGray'] = ToGray
+aug_dict['ToSepia'] = ToSepia
+aug_dict['GaussNoise'] = GaussNoise
+aug_dict['Normalize'] = Normalize
+#Cutout
+aug_dict['GridDropout'] = GridDropout
+aug_dict['Cutout'] = Cutout
 
 aug_dict['MotionBlur'] = A.MotionBlur(blur_limit = 11, p = 1.0)
 aug_dict['Perspective'] = A.Perspective(scale = 0.3, p = 1.0)
@@ -56,3 +57,4 @@ for run_name in aug_dict.keys():
     augmentation = aug_dict[run_name]
     create_augmented_images(augmentation = augmentation, olddir = OLDDIR, newdir = NEWDIR, maskdir = MASKDIR, data_info_path = DATA_INFO)
     os.system('python3 train.py --data dataset.yaml --hyp custom_hyp.yaml --weights yolov5s.pt --cache --epochs 500 --run_name {}'.format(run_name))
+    time.sleep(60)
